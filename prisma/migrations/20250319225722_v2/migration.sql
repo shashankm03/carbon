56950fed2f14a41ef0723d9651bb3f16f7851ebe
shashankm03 <shashankmahaladkar@gmail.com>
2025-03-20 04:29:50 +0530

    refactor: update date fields to string type in models and enhance API with CRUD operations for students and professors

---
+++ prisma/migrations/20250319225722_v2/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "LibraryMembership" ALTER COLUMN "issueDate" SET DATA TYPE TEXT,
+ALTER COLUMN "expiryDate" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Student" ALTER COLUMN "dateOfBirth" SET DATA TYPE TEXT;
